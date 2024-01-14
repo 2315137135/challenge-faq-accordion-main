581696673e5616c77f0ac8365a27f65a21198134
yunxi <2315137135@qq.com>
2024-01-14 12:21:57 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Accordion from './components/Accordion.vue'
+
+</script>
+
+<template  >
+    <div class="p-8 mx-8 bg-white rounded-xl max-w-96 md:max-w-screen-sm bg">
+        <div class="flex gap-4 mb-8">
+            <img src="/src/assets/images/icon-star.svg" class="inline">
+            <h1 class="inline">FAQs</h1>
+        </div>
+        <ul>
+            <Accordion>
+                <template #title>
+                    What is Frontend Mentor, and how will it help me?
+                </template>
+                Frontend Mentor offers realistic coding challenges to help developers improve their
+                frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+                all levels and ideal for portfolio building.
+            </Accordion>
+            <Accordion>
+                <template #title>
+                    Is Frontend Mentor free?
+                </template>
+                Yes, Frontend Mentor offers both free and premium coding challenges, with the free
+                option providing access to a range of projects suitable for all skill levels.
+            </Accordion>
+
+            <Accordion>
+                <template #title>
+                    Can I use Frontend Mentor projects in my portfolio?
+                </template>
+                Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+                way to showcase your skills to potential employers!
+            </Accordion>
+
+
+            <Accordion>
+                <template #title>
+                    How can I get help if I'm stuck on a Frontend Mentor challenge?
+                </template>
+                The best place to get help is inside Frontend Mentor's Discord community. There's a help
+                channel where you can ask questions and seek support from other community members.
+            </Accordion>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,52 @@
+<template>
+    <li class="flex flex-col py-6 ">
+        <div class="relative font-bold text-md hover:text-purple-600">
+            <!-- title -->
+            <button @click="toogleOpen">
+                <div class="pr-16 text-left">
+                    <slot name="title"></slot>
+                </div>
+                <img class="absolute top-0 bottom-0 right-0 my-auto" :src="butonImg" />
+            </button>
+        </div>
+        <div class="pr-8 overflow-hidden transition-all duration-300 max-h-0 text-neutral-500" :class="{ 'open': isOpen }">
+            <!-- content -->
+            <br>
+            <slot></slot>
+        </div>
+
+    </li>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import minus from "@/assets/images/icon-minus.svg"
+import plus from "@/assets/images/icon-plus.svg"
+
+let isOpen = ref(false)
+
+let butonImg = computed(() => {
+    return isOpen.value ? minus : plus
+})
+
+let toogleOpen = () => {
+    isOpen.value = !isOpen.value
+}
+
+</script>
+
+<style scoped>
+.open {
+    max-height: 6rem;
+}
+
+
+
+li {
+    @apply border-t border-gray-100
+}
+
+li:first-child {
+    @apply border-opacity-0
+}
+</style>
